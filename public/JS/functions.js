eba0f130474b0ff60c34d05f006a654214ab754e
MartinGRamirezH06 <martin.ramirezhilario@gmail.com>
2025-12-22 01:10:04 -0600

    Ya funciona el boton de agregar, tambien ya se ve en la vista y muestra la cantidad de objetos que hay en la lista

--- public/JS/functions.js
+++ public/JS/functions.js
@@ -3,19 +3,20 @@ const iconCart=header.firstElementChild;
 
 const cart=document.querySelector(".cart")
 console.log(cart)
-//Funcion para abrir el carrito
+//--------------Funcion para abrir el carrito---------------
 iconCart.addEventListener("click",() =>{
     cart.classList.toggle("--showCart");
     console.log("El carrito ya aparecio");
 });
-//Funcion para cerrar el carrito
+//-------------------------
+//-----------Funcion para cerrar el carrito--------------
 const iconCloseCart=cart.querySelector(".--selectorClose");
 console.log(iconCloseCart);
 iconCloseCart.addEventListener("click",() => {
     cart.classList.replace("--showCart","--closeCart");
     console.log("Ya se cerro el carrito");
 });
-//Clase para cada articulo
+//---------Clase para cada articulo-----------------------
 const listaCarrito=[]
 
 class articulo{
@@ -28,14 +29,64 @@ class articulo{
         listaCarrito.push(this);
         listaCarrito.forEach(function(a=this){console.dir(a)})
         // console.log("Se añadio a carrito: "+listaCarrito);
+        actualizarVista();
+    }
+    eliminarDelCarrito(){
+        
     }
 }
-//Metodo para que funcionen los botones de agregar
+//------------------------------------
+
+//-----Funcion para actualizar la vista de html
+const actualizarVista= () => {
+    const carritoContenedor=document.querySelector(".__container-items");
+    carritoContenedor.innerHTML="";
+    listaCarrito.forEach(producto=>{
+        const nuevaFila=document.createElement("div");
+        nuevaFila.classList.add("__container-Product");
+
+        const imgProducto=document.createElement("img");
+        const nombreProducto=document.createElement("p");
+        const precioProducto=document.createElement("p");
+        const iconEliminar=document.createElement("i");
+        const imgEliminar=document.createElement("img");
+
+        imgProducto.src=producto.urlImagen;
+        nombreProducto.textContent=producto.name;
+        precioProducto.textContent=producto.price;
+        precioProducto.classList.add("__price");
+        iconEliminar.classList.add("--selectorDelete");
+        imgEliminar.src= "/img/iconoDelete.png";
+        imgEliminar.classList.add("__delete-icon");
+        iconEliminar.append(imgEliminar)
+        nuevaFila.append(imgProducto,nombreProducto,precioProducto,iconEliminar);
+        carritoContenedor.append(nuevaFila);
+    });   
+}//--------------------------------------------------------------
+//Funcion para contar los elementos del carrito
+const contarElementos =()=>{
+    const contador=document.querySelector(".__contador");
+    contador.textContent=listaCarrito.length;
+}
+//---------------------------------
+//-------Funcion para eliminar objeto en lista en la vista
+const iconRemoveCart=document.querySelectorAll(".--selectorDelete");
+iconRemoveCart.forEach(producto =>{
+    producto.addEventListener("click",()=>{
+        const raizProducto=producto.parentElement;
+        raizProducto.remove();
+    })
+})
+//---------------------------
+//--------Metodo para que funcionen los botones de agregar------------------
 document.querySelectorAll(".__buttonAgregar").forEach(buton => {
     buton.addEventListener("click",()=>{
         const producto=buton.closest(".__item");
         const articuloSeleccionado=new articulo(producto);
         articuloSeleccionado.agregarACarrito();
+        actualizarVista();
+        contarElementos();
+
     });
 });
-
+//------------------------------------------------------
